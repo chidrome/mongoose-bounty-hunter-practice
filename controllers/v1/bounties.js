87dc4d4301f859a3f703f6b647bd24803855c0b0
chidrome <chidrome@gmail.com>
2019-01-29 11:57:13 -0800

    added routes

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -16,5 +16,43 @@ router.get('/', (req, res)=>{
     })
 })
 
+router.post('/', (req, res) => {
+    // Array data is sent as a string; parse it
+    req.body.hunters = JSON.parse(req.body.hunters);
+    db.Bounty.create(req.body)
+    .then(bounty => {
+        res.status(201).send(bounty)
+    })
+    .catch(error => {
+        console.log('ERROR Creating', error)
+        res.status(500).send({ message: 'Server Error' });
+    })
+})
+
+router.get('/:id', (req, res) => {
+    db.Bounty.findById(req.params.id)
+    .then(bounty => {
+        res.send(bounty)
+    })
+    .catch(error => {
+        console.log('ERROR FINDING ID', error);
+        res.status(500).send({ message: 'Server Error' });
+    });
+})
+
+router.delete('/:id', (req, res) => {
+    db.Bounty.findOneAndDelete({
+        _id: req.params.id
+    })
+    .then(() => {
+        res.status(204).send({ message: 'Successful Deletion' })
+    })
+    .catch(error => {
+        console.log('ERROR FINDING ID', error);
+        res.status(500).send({ message: 'Server Error' });
+    })
+})
+
+
 
 module.exports = router;
