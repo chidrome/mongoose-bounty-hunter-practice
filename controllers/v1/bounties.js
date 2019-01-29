936d588de81ca7382e8f0d71621b951b3a6c01f6
chidrome <chidrome@gmail.com>
2019-01-29 12:32:01 -0800

    added a put/edit route

--- controllers/v1/bounties.js
+++ controllers/v1/bounties.js
@@ -40,6 +40,27 @@ router.get('/:id', (req, res) => {
     });
 })
 
+router.put('/:id', (req, res) => {
+    // Array data is sent as a string; parse it
+    req.body.hunters = JSON.parse(req.body.hunters)
+    db.Bounty.findOneAndUpdate({
+        _id: req.params.id
+    },
+        req.body,
+        {
+            new: true // this allows to return the updated object
+        }
+    )
+    .then(bounty => {
+        res.send(bounty);
+    })
+    .catch(error => {
+        console.log('Error Updating Item', error);
+        res.status(500).send({ message: 'Server Error' });
+    })
+
+})
+
 router.delete('/:id', (req, res) => {
     db.Bounty.findOneAndDelete({
         _id: req.params.id
